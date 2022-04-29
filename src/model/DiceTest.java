072e61ed7e802677071c73702333f5b7fbc68e84
Jeferson Morales Mariciano <moralj@usi.ch>
2022-04-29 11:26:50 +0200

    gitignore updated

---
+++ src/model/DiceTest.java
@@ -0,0 +1,44 @@
+package model;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class DiceTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class DiceTest
+{
+    /**
+     * Default constructor for test class DiceTest
+     */
+    public DiceTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
