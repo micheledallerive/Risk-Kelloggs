eb8e144c1dfa9dfdf701a395015517ddb671cabb
Michele Dalle Rive <dallem@usi.ch>
2022-05-06 10:57:27 +0200

    Remove UNIX shell colors from AryColor

--- src/model/enums/ArmyColor.java
+++ src/model/enums/ArmyColor.java
@@ -5,36 +5,10 @@ package model.enums;
  * @author dallem@usi.ch
  */
 public enum ArmyColor {
-    RED("\033[0;31m"),
-    BLACK("\033[0;30m"),
-    BLUE("\033[0;34m"),
-    GREEN("\033[0;32m"),
-    YELLOW("\033[0;33m"),
-    PINK("\033[0;35m");
-
-    private String colorCode;
-
-    /**
-     * Creates an ArmyColor with the given color code.
-     * @param code the UNIX shell color code.
-     */
-    ArmyColor(String code) {
-        this.colorCode = code;
-    }
-
-    /**
-     * Returns the UNIX shell color code for the armycolor.
-     * @return the UNIX color code
-     */
-    public String getColorCode() {
-        return this.colorCode;
-    }
-
-    /**
-     * Returns the UNIX shell reset color code.
-     * @return returns the reset color code.
-     */
-    public String getReset() {
-        return "\033[0m";
-    }
+    RED,
+    BLACK,
+    BLUE,
+    GREEN,
+    YELLOW,
+    PINK
 }
