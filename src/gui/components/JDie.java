e6276414f4de44f42e5a61d5c9d0a8fa1b3247c3
Jeferson Morales Mariciano <moralj@usi.ch>
2022-05-26 23:40:51 +0200

    added finals, constants, organizing JDie

--- src/gui/components/JDie.java
+++ src/gui/components/JDie.java
@@ -2,37 +2,51 @@ package gui.components;
 
 import model.Die;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.Timer;
+import java.awt.Graphics;
 
 public class JDie extends JComponent {
+    //region CONSTANTS
+    private static final String PATH = "src/gui/assets/images/die/white/";
+    private static final String EXT = ".png";
+    private static final byte SIZE = 64;
+    private static final byte FACES = 6;
+    //endregion
+
+    //region FIELDS
     private static final ImageIcon[] images = new ImageIcon[6];
-    private int animation = 0;
-
-    public static void init() {
-        for (int i = 0; i < 6; i++) {
-            images[i] = new ImageIcon("src/gui/assets/images/die/white/" + (i+1) + ".png");
-        }
-    }
-
     private Timer timer;
+    private int animation = 0;
     private int value;
+    //endregion
 
+    //region CONSTRUCTORS
     public JDie() {
         this(250, 5000);
     }
 
-    public JDie(int frameDuration, int totalDuration) {
-        setSize(64,64);
+    public JDie(final int frameDuration, final int totalDuration) {
+        this.setSize(SIZE, SIZE);
         this.timer = new Timer(frameDuration, e -> {
-            this.value = (int) (Math.random() * 6);
-            if(animation == totalDuration/frameDuration) {
-                animation = value-1;
-                stop();
+            this.value = (int) (Math.random() * FACES);
+            if (this.animation == totalDuration/frameDuration) {
+                this.animation = this.value - 1;
+                this.timer.stop();
             } else {
-                animation++;
+                this.animation++;
             }
             this.repaint();
         });
+    }
+    //endregion
+
+    //region METHODS
+    public static final void init() {
+        for (int i = 0; i < FACES; i++) {
+            images[i] = new ImageIcon(PATH + (i + 1) + EXT);
+        }
     }
 
     public void start() {
@@ -40,14 +54,10 @@ public class JDie extends JComponent {
         Die.casualRoll();
     }
 
-    public void stop() {
-        this.timer.stop();
-    }
-
     @Override
-    protected void paintComponent(java.awt.Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(images[Die.casualRoll() - 1].getImage(), 0, 0, this);
     }
-
+    //endregion
 }
