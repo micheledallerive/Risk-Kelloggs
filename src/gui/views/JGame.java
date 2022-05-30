189f1feb4e854347e81b004fdc5cf9a1760668e6
Jeferson Morales Mariciano <moralj@usi.ch>
2022-05-30 19:57:12 +0200

    fix

--- src/gui/views/JGame.java
+++ src/gui/views/JGame.java
@@ -25,6 +25,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.function.Function;
